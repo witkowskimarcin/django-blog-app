fcea8dc27f20f756d29894422aa0ac854c54fb1d
Marcin Witkowski <witkowskimarcin@hotmail.com>
2019-12-15 03:21:53 +0100

    - add model Post - attach module TinyMCE to project

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/date", {'fields': ["post_title", "post_published"]}),
+        ("Content", {"fields": ["post_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
+
+admin.site.register(Post,PostAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_content = models.TextField()
+    post_published = models.DateTimeField('date published', default=datetime.now)
+
+    def __str__(self):
+        return self.post_title
+
+# class Tag(models.Model):
+#     tag_name = models.CharField(max_length=20)
+
+#     def __str__(self):
+#         return '#'+self.tag_name
